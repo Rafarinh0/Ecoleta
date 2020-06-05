7476e41c68e53e020b4aa8dad49a69c52c07e82f
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-06-05 11:29:33 -0300

    Busca de itens de coleta da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+//sempre que a gente cria um estado pra um array ou um objeto,
+//precisamos, manualmente, informar o tipo da variável que vai ser armazenada!
+
+interface Item {
+    id: number;
+    name: string;
+    image_url: string;
+}
+
 const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -69,7 +87,7 @@ const CreatePoint = () => {
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
 
-                        <Marker position={[-8.0646418, -34.8955188]}/>
+                        <Marker position={[-8.0646418, -34.8955188]} />
                     </Map>
 
                     <div className="field-group">
@@ -96,30 +114,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.name} />
+                                <span>{item.name}</span>
+                            </li>
+                        ))}
                     </ul>
                 </fieldset>
 
